440c369d90f50ab5b86dfb60cf9430d2b6c7dad3
armandovanwyk <armando@dataforgeconsulting.com>
2025-03-09 10:53:51 +0200

    ...

---
+++ SmartHydroponicController/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHydroponicController;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SmartHydroponicController/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartHydroponicController;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
